cd9431eaddbdb9c2975abbcbaab39e868fc4853d
dericward <dericward@hotmail.com>
2023-02-22 02:07:15 +0000

    got JQuery generating correct-looking time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,6 @@
 
 $("#currentDay").html(moment().toString());
 
-$(".container").append('<div class="row time-block">');
-$(".container").append('<div class="col-2 hour">7AM</div>');
-$(".container").append('<textarea class="col-8 past"></textarea>');
-$(".container").append('<button class="col-2 saveBtn"></button>');
+$(".container").append('<div class="row time-block"><div class="col-2 hour">8AM</div><textarea class="col-8 past"></textarea><button class="col-2 saveBtn"></button></div>');
+$(".container").append('<div class="row time-block"><div class="col-2 hour">1PM</div><textarea class="col-8 present"></textarea> <button class="col-2 saveBtn"></button></div>');
+$(".container").append('<div class="row time-block"><div class="col-2 hour">4PM</div><textarea class="col-8 future"></textarea> <button class="col-2 saveBtn"></button></div>');
