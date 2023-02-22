3262677d9a1cc36b225fceeb384769bf7c32fe51
dericward <dericward@hotmail.com>
2023-02-22 01:25:26 +0000

    started adding JQuery DOM updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 // must use Jquery and Moment.js
 
-let todaysDate = moment().toString();
-document.getElementById("currentDay").innerHTML = todaysDate;
-// $("#currentDay").textContent = todaysDate;
-console.log(todaysDate);
+// let todaysDate = moment().toString();
+// document.getElementById("currentDay").innerHTML = todaysDate;
+// $("#currentDay").html(todaysDate);
+
+$("#currentDay").html(moment().toString());
+
+$(".container").append('<div class="row time-block">');
+$(".container").append('<div class="col-2 hour">7AM</div>');
+$(".container").append('<textarea class="col-8 past"></textarea>');
+$(".container").append('<button class="col-2 saveBtn"></button>');
