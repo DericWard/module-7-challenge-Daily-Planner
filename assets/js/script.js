7bf232989ef6cace8b7091d4c65564f0414f6dc8
dericward <dericward@hotmail.com>
2023-02-24 20:05:59 +0000

    now storing input to local storage and retrieving from local storage and displaying in correct fields

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,30 @@
-// MUST use Jquery and Moment.js
+// MUST use JQUERY and MOMENT.JS
 
-function saveUserInput() {
+function getPlannerEntries(plannerEntries) {
+    plannerEntries = JSON.parse(localStorage.getItem("plannerEntries")); 
+    plannerEntries.forEach(function(timeBlockEntry) {
+        console.log("timeblock:" + timeBlockEntry.timeSlot + " : " + "text: " + timeBlockEntry.text);
+        $(`.${timeBlockEntry.timeSlot}`).append(` ${timeBlockEntry.text}`)
+    });
 };
-function displayTimeBlockText() {
-};
-function getFromLocalStorage() {
-    displayTimeBlockText();
+
+// save the new planner entry to a storage array, create if doesn't exit. Create object and push values to array, send to local storage
+function savePlannerEntry(btnID, textEntry) {
+        let plannerEntries = JSON.parse(localStorage.getItem("plannerEntries")) || [];
+        let newPlannerEntry = {
+            timeSlot: btnID,
+            text: textEntry
+        };
+    plannerEntries.push(newPlannerEntry);
+    localStorage.setItem("plannerEntries", JSON.stringify(plannerEntries));
+    getPlannerEntries(plannerEntries);
 };
 
 // generate time-blocks and colour the time-block textarea sections to reflect past/present/future time
 function generateTimeBlocks() { 
     let now = moment().format('H');
     for(let i = '09'; i < 18; i++ ) {
-        $(".container").append(`<div class="row time-block"><div class="col-2 hour">${i}:00</div><textarea class="col-8" id="${i}"></textarea><button class="col-2 saveBtn" id="btnID-${i}"></button></div>`);
+        $(".container").append(`<div class="row time-block"><div class="col-2 hour">${i}:00</div><textarea class="col-8 ${i}" id="text${i}"></textarea><button class="col-2 saveBtn" id="${i}"></button></div>`);
         if ((i - now) < 0) {
             $("textarea").addClass("future");
         }
@@ -21,7 +33,7 @@ function generateTimeBlocks() {
         }
         else $("textarea").addClass("past");
     };
-    getFromLocalStorage();
+    getPlannerEntries();
 };
 
 // display today's date and a real-time clock
@@ -36,18 +48,35 @@ function displayDateAndClock() {
 
 displayDateAndClock();
 
+$("button").click(function() {
+    let btnID = $(this).attr("id");
+    let textEntry = $(`#text${btnID}`).val();
+    savePlannerEntry(btnID, textEntry);
+});
+
+
+
+
+
+    // console.log("button-id:", $(this).attr("id"));
+    // console.log("textarea-text:", $(`#text${btnID}`).val());
+    // console.log("textarea-text:", textEntry);
+
+// $("textarea").click(function() {
+//     console.log("textarea-text:", $(this).val());
+// });
+
 // let timeBlockContainer = $(".container");
 
+// $(".saveBtn").on("click", function() {
 
-$(".saveBtn").on("click", function() {
+//     $("button").each(function(index, element) { 
+//         console.log("button index: " + index + "    button ID: " + $(element).attr("id"));
+//     });
 
-    $("button").each(function(index, element){ 
-        console.log("button index: " + index + "    button ID: " + $(element).attr("id"));
-    });
-
-    $("textarea").each(function(index, element) {
-        console.log("textarea index: " + index +    ". ID: " + $(element).attr("id") +  ". text: " + $(element).val());
-    });
+//     $("textarea").each(function(index, element) {
+//         console.log("textarea index: " + index +    ". ID: " + $(element).attr("id") +  ". text: " + $(element).val());
+//     });
 
     // let plannerText = $("textarea").val();
     // let plannerTextID = $("textarea").attr("id");
@@ -58,7 +87,7 @@ $(".saveBtn").on("click", function() {
     // console.log(`plannerTextID: ${plannerTextID}`);
 
     // saveUserInput();
-});
+// });
 
 // add index and $(element).val() to an array
 // save the text input to local storage
